3a4a9accc6fcb806ce7090f11517d8a3bbfbb46c
Matthew Rowe <mrowe.w@gmail.com>
2018-05-20 16:33:07 -0400

    update knex file with production settings

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/test-web-store'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
